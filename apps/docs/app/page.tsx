cba74936e3bbf2da0da5a88df2bcfc565c9debcc
Junhee <59816563+wjunhee102@users.noreply.github.com>
2024-04-26 20:09:59 +0900

    [refactor] rename
    * [refactor] 디렉토리 위치 변경
    
    * [refactor] rename
    : react-libs -> react-crates

--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -1,10 +1,7 @@
-import { Button, Header } from "ui";
-
 export default function Page() {
   return (
-    <>
-      <Header text="Docs" />
-      <Button />
-    </>
+    <header>
+      <button>button</button>
+    </header>
   );
 }
