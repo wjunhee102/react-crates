203295ca339396aecc9961b93ff50fb9f612fedf
junhee <wjunhee102@gmail.com>
2024-05-27 19:17:31 +0900

    [docs/fix] github-actions build error fix

--- apps/docs/src/pages/index.tsx
+++ apps/docs/src/pages/index.tsx
@@ -17,7 +17,7 @@ function HomepageHeader() {
           as="h1"
           className="hero__title"
           onClick={() => {
-            modalCtrl.alert("Hello world!");
+            modalCtrl.alert("Hello world");
           }}
         >
           {siteConfig.title}
