a4bebed095b274f89a9d04b1695a7a2ae1d82107
junhee <wjunhee102@gmail.com>
2024-05-27 19:25:22 +0900

    [docs/feat] ModalProvider

--- apps/docs/src/components/layout/Layout.tsx
+++ apps/docs/src/components/layout/Layout.tsx
@@ -1,11 +1,11 @@
-// import { ModalProvider } from "@site/src/libs/modal";
+import { ModalProvider } from "@site/src/libs/modal";
 import { default as OriginLayout, Props } from "@theme/Layout";
 
 export default function Layout({ children, ...restProps }: Props) {
   return (
     <OriginLayout {...restProps}>
       {children}
-      {/* <ModalProvider /> */}
+      <ModalProvider />
     </OriginLayout>
   );
 }
