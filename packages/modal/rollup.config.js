2cebe2d745fc72eaa55225b5c16b5c38d2de6e2f
junhee <wjunhee102@gmail.com>
2024-05-27 18:16:41 +0900

    logger

--- packages/modal/rollup.config.js
+++ packages/modal/rollup.config.js
@@ -7,6 +7,30 @@ import postcss from "rollup-plugin-postcss";
 import terser from "@rollup/plugin-terser";
 import babel from "@rollup/plugin-babel";
 
+function myLogger() {
+  return {
+    name: "my-logger", // 이 플러그인의 이름
+    buildStart() {
+      console.log("Build starting...");
+    },
+    load(id) {
+      console.log(`Loading ${id}`);
+      return null; // 다른 로드 처리를 방해하지 않도록 null 반환
+    },
+    transform(code, id) {
+      console.log(`Transforming ${id}`);
+      return null; // 코드를 변형하지 않고 원본 그대로 반환
+    },
+    buildEnd(error) {
+      if (error) {
+        console.log("Build failed:", error);
+      } else {
+        console.log("Build finished successfully.");
+      }
+    },
+  };
+}
+
 const packageJson = require("./package.json");
 
 export default [
@@ -58,7 +82,7 @@ export default [
         format: "es",
       },
     ],
-    plugins: [dts()],
+    plugins: [dts.default(), myLogger],
     external: [/\.css$/, /\.scss$/],
   },
 ];
