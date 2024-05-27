c78df1d8661a5350d0d93d9f7dab7e0a47497a7b
junhee <wjunhee102@gmail.com>
2024-05-27 19:10:10 +0900

    [docs/chage] modal content change

--- apps/docs/src/pages/index.tsx
+++ apps/docs/src/pages/index.tsx
@@ -17,7 +17,7 @@ function HomepageHeader() {
           as="h1"
           className="hero__title"
           onClick={() => {
-            modalCtrl.alert("alert");
+            modalCtrl.alert("Hello world!");
           }}
         >
           {siteConfig.title}
