aca6140b84b0147dd7bdcfcf4c13075d2548d1b8
junhee <wjunhee102@gmail.com>
2024-05-31 23:34:28 +0900

    [modal/fix] version 0.5.8
    - modal bottom 포지션 열릴때 tab 누르면 깜박거리는 현상 수정

--- packages/modal/src/components/providers/ModalComponentProvider.tsx
+++ packages/modal/src/components/providers/ModalComponentProvider.tsx
@@ -63,126 +63,132 @@ const ModalComponentProvider = ({
     [isOpened]
   );
 
-  const {
-    actionBackCover,
-    actionToEsc,
-    disableOutsideFocus,
-    focusModalContent,
-    focusBackCover,
-  } = useMemo(
-    () => ({
-      actionBackCover(event: MouseEvent<HTMLButtonElement>) {
-        if (modal.options.backCoverConfirm === null || event.detail === 0) {
-          return;
-        }
+  const { actionBackCover, actionToEsc, disableOutsideFocus, focusBackCover } =
+    useMemo(
+      () => ({
+        actionBackCover(event: MouseEvent<HTMLButtonElement>) {
+          if (modal.options.backCoverConfirm === null || event.detail === 0) {
+            return;
+          }
 
-        modal.action(modal.options.backCoverConfirm);
-      },
-      actionToEsc(event: KeyboardEvent<HTMLDivElement>) {
-        if (
-          event.key !== "Escape" ||
-          modal.options.backCoverConfirm === null ||
-          !isEscKeyActive
-        ) {
-          return;
-        }
+          modal.action(modal.options.backCoverConfirm);
+        },
+        actionToEsc(event: KeyboardEvent<HTMLDivElement>) {
+          if (
+            event.key !== "Escape" ||
+            modal.options.backCoverConfirm === null ||
+            !isEscKeyActive
+          ) {
+            return;
+          }
 
-        isEscKeyActive && modal.action(modal.options.backCoverConfirm);
-      },
-      disableOutsideFocus(event: KeyboardEvent<HTMLDivElement>) {
-        if (event.key === "Escape") {
+          isEscKeyActive && modal.action(modal.options.backCoverConfirm);
+        },
+        disableOutsideFocus(event: KeyboardEvent<HTMLDivElement>) {
+          if (event.key === "Escape") {
+            event.preventDefault();
+          }
+
+          if (event.key !== "Tab") {
+            return;
+          }
+
           event.preventDefault();
-        }
 
-        if (event.key !== "Tab") {
-          return;
-        }
+          if (!modalContentRef.current) {
+            return;
+          }
 
+          if (
+            focusableElements.current.first === null ||
+            focusableElements.current.last === null
+          ) {
+            modalContentRef.current.focus();
+
+            return;
+          }
+
+          if (
+            event.target === focusableElements.current.first &&
+            event.shiftKey
+          ) {
+            focusableElements.current.last.focus();
+            focusableElements.current = {
+              ...focusableElements.current,
+              index: focusableElements.current.elements.length - 1,
+            };
+
+            return;
+          }
+
+          if (
+            event.target === focusableElements.current.last &&
+            !event.shiftKey
+          ) {
+            focusableElements.current.first.focus();
+            focusableElements.current = {
+              ...focusableElements.current,
+              index: 0,
+            };
+
+            return;
+          }
+
+          focusableElements.current = {
+            ...focusableElements.current,
+            index: focusableElements.current.index + 1,
+          };
+
+          const targetElement =
+            focusableElements.current.elements[focusableElements.current.index];
+
+          if (!targetElement) {
+            focusableElements.current = {
+              ...focusableElements.current,
+              index: 0,
+            };
+            modalContentRef.current.focus();
+
+            return;
+          }
+
+          targetElement.focus();
+        },
+        focusBackCover() {
+          modalContentRef.current && modalContentRef.current.focus();
+        },
+      }),
+      []
+    );
+
+  const focusModalContent = useCallback(
+    (event: FocusEvent<HTMLDivElement>) => {
+      if (!isOpened) {
         event.preventDefault();
+        modalRef.current && modalRef.current.focus();
 
-        if (!modalContentRef.current) {
-          return;
-        }
+        return;
+      }
 
-        if (
-          focusableElements.current.first === null ||
-          focusableElements.current.last === null
-        ) {
-          modalContentRef.current.focus();
+      if (
+        event.currentTarget.contains(event.target) &&
+        event.currentTarget !== event.target
+      ) {
+        return;
+      }
 
-          return;
-        }
+      if (modal.onOpenAutoFocus) {
+        modal.onOpenAutoFocus(event);
 
-        if (
-          event.target === focusableElements.current.first &&
-          event.shiftKey
-        ) {
-          focusableElements.current.last.focus();
-          focusableElements.current = {
-            ...focusableElements.current,
-            index: focusableElements.current.elements.length - 1,
-          };
+        return;
+      }
 
-          return;
-        }
+      event.preventDefault();
 
-        if (
-          event.target === focusableElements.current.last &&
-          !event.shiftKey
-        ) {
-          focusableElements.current.first.focus();
-          focusableElements.current = {
-            ...focusableElements.current,
-            index: 0,
-          };
-
-          return;
-        }
-
-        focusableElements.current = {
-          ...focusableElements.current,
-          index: focusableElements.current.index + 1,
-        };
-
-        const targetElement =
-          focusableElements.current.elements[focusableElements.current.index];
-
-        if (!targetElement) {
-          focusableElements.current = {
-            ...focusableElements.current,
-            index: 0,
-          };
-          modalContentRef.current.focus();
-
-          return;
-        }
-
-        targetElement.focus();
-      },
-      focusModalContent(event: FocusEvent<HTMLDivElement>) {
-        if (
-          event.currentTarget.contains(event.target) &&
-          event.currentTarget !== event.target
-        ) {
-          return;
-        }
-
-        if (modal.onOpenAutoFocus) {
-          modal.onOpenAutoFocus(event);
-
-          return;
-        }
-
-        event.preventDefault();
-
-        focusableElements.current.first &&
-          focusableElements.current.first.focus();
-      },
-      focusBackCover() {
-        modalContentRef.current && modalContentRef.current.focus();
-      },
-    }),
-    []
+      focusableElements.current.first &&
+        focusableElements.current.first.focus();
+    },
+    [isOpened]
   );
 
   useEffect(() => {
