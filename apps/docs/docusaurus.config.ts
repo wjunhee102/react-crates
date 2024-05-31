12205bc1b098c136b93c262987a694ae1dd46253
junhee <wjunhee102@gmail.com>
2024-05-31 16:16:31 +0900

    [docs/feat] tailwindCSS 추가

--- apps/docs/docusaurus.config.ts
+++ apps/docs/docusaurus.config.ts
@@ -1,6 +1,7 @@
 import { themes as prismThemes } from "prism-react-renderer";
 import type { Config } from "@docusaurus/types";
 import type * as Preset from "@docusaurus/preset-classic";
+import tailwindPlugin from "./tailwind-config.cjs";
 
 const config: Config = {
   title: "@react-crates/modal",
@@ -53,7 +54,7 @@ const config: Config = {
       } satisfies Preset.Options,
     ],
   ],
-
+  plugins: [tailwindPlugin],
   themeConfig: {
     // Replace with your project"s social card
     image: "img/docusaurus-social-card.jpg",
---
+++ apps/docs/tailwind.config.js
@@ -0,0 +1,21 @@
+const { fontFamily } = require("tailwindcss/defaultTheme");
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  corePlugins: {
+    preflight: false,
+    container: false,
+  },
+  darkMode: ["class", '[data-theme="dark"]'],
+  content: ["./src/**/*.{jsx,tsx,html}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['"Inter"', ...fontFamily.sans],
+        jakarta: ['"Plus Jakarta Sans"', ...fontFamily.sans],
+        mono: ['"Fira Code"', ...fontFamily.mono],
+      },
+    },
+  },
+  plugins: [],
+};
